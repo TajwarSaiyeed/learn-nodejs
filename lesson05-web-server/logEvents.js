dfa7dcf0984fdd34c7d5698df0ea45d4a4bb0a10
tajwarsaiyeed <rjabid36@gmail.com>
2024-08-02 15:47:23 +0600

    chore: web-server using nodejs

---
+++ lesson05-web-server/logEvents.js
@@ -0,0 +1,31 @@
+const { format } = require("./node_modules/date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs").promises;
+
+const logEvents = async (message,logName) => {
+  const dateTime = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+  const logMessage = `${dateTime}\t${uuid()}\t${message}`;
+
+  // console.log("🚀 ~ logEvents ~ logMessage", logMessage);
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", logName),
+      logMessage + "\n"
+    );
+
+    console.log("Event logged successfully");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = logEvents;
